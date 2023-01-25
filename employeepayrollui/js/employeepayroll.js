f7a73a66899fb4040f6787bd94f10edfeac54a3f
PriyankaShinde125 <pkshinde125@gmail.com>
2023-01-25 13:19:11 +0530

    ADDED : Code to validate name

--- employeepayrollui/js/employeepayroll.js
+++ employeepayrollui/js/employeepayroll.js
@@ -1,6 +1,19 @@
 const salary = document.querySelector('#salaryrange');
 const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
-salary.addEventListener('input', function() {
+salary.addEventListener('input', function () {
     output.textContent = salary.value;
+});
+
+const nameText = document.querySelector('#name');
+const errorText = document.querySelector('.text-error');
+nameText.addEventListener('input' , function() {
+    let namePattern = new RegExp(/^[A-Z]{1}[a-zA-z\\s]{2,}/);
+    if (namePattern.test(nameText.value)) {
+        errorText.textContent = "";
+    }
+    else if(nameText.value == ""){
+        errorText.textContent = "";
+    }else 
+    errorText.textContent = "Invalid Name";
 });
