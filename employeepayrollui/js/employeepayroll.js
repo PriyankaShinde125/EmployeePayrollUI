fd297bfa523fc739905c5ecc57ce01de3baf2140
PriyankaShinde125 <pkshinde125@gmail.com>
2023-01-25 16:16:32 +0530

    ADDED : Code to populate form data to payroll object

--- employeepayrollui/js/employeepayroll.js
+++ employeepayrollui/js/employeepayroll.js
@@ -1,3 +1,4 @@
+window.addEventListener('DOMContentLoaded', (event) => {
 const salary = document.querySelector('#salaryrange');
 const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
@@ -17,3 +18,42 @@ nameText.addEventListener('input' , function() {
     }else 
     errorText.textContent = "Invalid Name";
 });
+});
+
+const save = () =>{
+    try {
+        let employeePayrollData = createEmployeePayroll();
+        console.log(employeePayrollData);
+    } catch (e) {
+        return e;
+    }
+}
+
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayrollData();
+    try {
+        employeePayrollData.name = document.querySelector('#name').value;
+    } catch (e) {
+        setTextValue('.textError',e);
+        throw e;
+    }
+
+employeePayrollData.profilePic = getSelectedValues('[name = profile]').pop();
+employeePayrollData.gender = getSelectedValues('[name = gender]').pop();
+employeePayrollData.department = getSelectedValues('[name = department]');
+employeePayrollData.salary = document.querySelector('#salaryrange').value;
+employeePayrollData.note = document.querySelector('#notes').value;
+let date = document.querySelector('#day').value + " " + document.querySelector('#month').value + " " + document.querySelector('#year').value;
+employeePayrollData.startDate = new Date(date);
+alert(employeePayrollData.toString());
+return employeePayrollData;
+}
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let setItems = [];
+    allItems.forEach(item => {
+        if(item.checked) setItems.push(item.value);
+    });
+    return setItems;
+}
